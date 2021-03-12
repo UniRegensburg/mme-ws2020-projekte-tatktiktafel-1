3ff08ae563b47f30b899db123c416d0d994a5d10
Jony <71759135+jonyschmid@users.noreply.github.com>
2021-03-12 17:17:08 +0100

    First Canvas
    Implemented the first drawable canvas.

--- app/resources/js/index.js
+++ app/resources/js/index.js
@@ -1,8 +1,40 @@
 /* eslint-env browser */
 
+import Config from "/app/resources/js/Config.js";
+
+var canvas = document.getElementById("canvas"),
+	ctx = canvas.getContext("2d"),
+	pos = {
+		x: 0,
+		y: 0,
+	};
+
+function setDrawPosition(event) {
+	pos.x = event.clientX - canvas.offsetLeft;
+	pos.y = event.clientY - canvas.offsetTop;
+}
+
+function draw(e) {
+	if (e.buttons !== 1) { //button 1 = linke maustaste, muss gedrückt sein
+		return;
+	}
+	ctx.beginPath(); // begin
+
+	ctx.lineWidth = Config.DRAW_DEFAULT_LINE_WIDTH;
+	ctx.lineCap = Config.DRAW_DEFAULT_LINE_CAP;
+	ctx.strokeStyle = Config.DRAW_DEFAULT_COLOR;
+
+	ctx.moveTo(pos.x, pos.y); // from
+	setDrawPosition(e);
+	ctx.lineTo(pos.x, pos.y); // to
+
+	ctx.stroke(); // draw
+}
+
 function init() {
-	let outputElement = document.querySelector("div");
-	outputElement.innerHTML = "It works!";
+	canvas.addEventListener("mousemove", draw, false);
+	canvas.addEventListener("mousedown", setDrawPosition, false);
+	canvas.addEventListener("mouseenter", setDrawPosition, false);
 }
 
 init();
